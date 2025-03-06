6c79c0706c0fd74070c4a507a2700c2acfac4719
hzhu3-hzhu <hzhu3@academic.rrc.ca>
2025-03-06 15:53:44 -0600

    Add routes for customers (index, alphabetized, missing_email)

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,14 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  root 'customers#index'
 
-  # root 'customers#index'
   get '/customers/alphabetized', to: 'customers#alphabetized', as: 'customers_alphabetized'
   get '/customers/missing_email', to: 'customers#missing_email', as: 'customers_missing_email'
+
   resources :customers, only: [:index]
-  root to: "home#index"
 
-
-  # Defines the root path route ("/")
-  # root "posts#index"
   ActiveAdmin.routes(self)
 end
