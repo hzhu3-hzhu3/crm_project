456476051d49f1dfeb485ae5f41699a2bb2f485f
hzhu3-hzhu <hzhu3@academic.rrc.ca>
2025-03-07 13:46:06 -0600

    Complete Part 1 and Part 2 of the CRM challenge

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,10 @@
+// app/assets/config/manifest.js
+
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_directory ../javascripts .js
+
 //= link active_admin.css
 //= link active_admin.js
+//= link application.js
+//= link controllers/hello_controller.js
+  //= link controllers/index.js
